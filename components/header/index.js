6af906ce4219cf1c3eccae8dc140ed87811dfe3a
gloriouspete <peterninyo4@gmail.com>
2025-02-25 00:47:44 +0100

    feed

--- components/header/index.js
+++ components/header/index.js
@@ -1,7 +1,4 @@
 import React, { useState } from "react";
-import { FaArrowRight, FaList, FaStop } from "react-icons/fa";
-import { GoBell } from "react-icons/go";
-import Box from "@/components/box/page";
 import { useRouter } from "next/navigation";
 import { MdOutlinePerson, MdOutlineSearch } from "react-icons/md";
 import { RiShoppingBag4Line } from "react-icons/ri";
@@ -9,14 +6,7 @@ import { RiShoppingBag4Line } from "react-icons/ri";
 
 export default function Header({ }) {
   const router = useRouter();
-  const [firstname, setfirstname] = useState("")
-  const [isClick, setisClick] = useState(false);
-  // Define the icon components
-  const eruku = () => <GoBell size={25} />;
 
-  const xa = () => <FaArrowRight size={32} color="#ffffff" />;
-
-  const [header, setHeader] = useState(eruku);
 
   return (
     <div className="w-full h-14 md:h-24 flex items-center justify-between bg-transparent px-6 border-black">
@@ -24,7 +14,7 @@ export default function Header({ }) {
         <img src="/psychiatry.png" className="rounded-full w-10 h-10 cursor-pointer" onClick={() => router.push("/profile")} />
         <p className="text-lg mx-2 font-prosto">Brand Name </p>
       </div>
-      {isClick && <Box />}
+     
       <div className=" flex items-center">
         <span className="w-auto h-full flex font-montserrat mx-2 font-medium" onClick={() => router.push("/profile")}>
           TEA COLLECTIONS
