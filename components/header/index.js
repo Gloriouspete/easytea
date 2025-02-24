740bcb915ed5ae30c1a8814d210adf76beb0a878
gloriouspete <peterninyo4@gmail.com>
2025-02-25 00:49:30 +0100

    fixed little bugs

--- components/header/index.js
+++ components/header/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useRouter } from "next/navigation";
 import { MdOutlinePerson, MdOutlineSearch } from "react-icons/md";
 import { RiShoppingBag4Line } from "react-icons/ri";
